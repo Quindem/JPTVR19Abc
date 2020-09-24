91bc060516b7da0198b8db835e904e1abea20281
pupil <pupil@ivkhk.ee>
2020-09-24 10:29:15 +0300

    готовая программа

--- src/jptvr19abc/App.java
+++ src/jptvr19abc/App.java
@@ -5,13 +5,50 @@
  */
 package jptvr19abc;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
  */
  class App {
      public void run(){
-         System.out.println("ABC");
+         System.out.println("Алфавит");
+         String a = "abcdefghijklmnopqrstuvwxyz";
+         boolean correct = true;
+        char[] abc = new char[26];
+        char[] abc2 = new char[26];
+        String str = new String();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        str = scanner.nextLine();
+        abc =a.toCharArray();
+        for(int i = 0; i < 26; i++)
+        {int index = str.indexOf(abc[i]);
+        if( index == -1  ){correct = false; abc2[i] = abc[i];
+        
+            
+            }
+        }
+        if( correct == true ){System.out.println("В строке все буквы");}
+        else{System.out.println("Не хватает букв " +  new String(abc2));}
+            
+        
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+            
+        
      }
+ }    
+         
+     
     
-}
+
